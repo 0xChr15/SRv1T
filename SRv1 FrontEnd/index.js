3a70bd598092ed808d75c5056fee3909fe43b035
0xChr15 <ayitzlegends34@gmail.com>
2023-02-05 04:28:30 -0500

    Added basic HTML infrastructure 4 UI inJavascript

---
+++ SRv1 FrontEnd/index.js
@@ -0,0 +1,19 @@
+//DOCTYPE html
+<html>
+<head>
+  <meta></meta> charset="UTF-8"
+  <title>Product UI</title>
+  <script src="index.js"></script>
+</head>
+<body>
+  <header>
+    <h1>Product UI</h1>
+  </header>
+  <main>
+  //Placeholder for UI elements
+  </main>
+  <footer>
+    <p>Copyright © 2023</p>
+  </footer>
+</body>
+</html>
