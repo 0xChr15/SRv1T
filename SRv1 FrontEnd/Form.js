53cdc9eeeb05923edd2bbd6b0bb298c4553e1c55
0xChr15 <ayitzlegends34@gmail.com>
2023-02-06 03:47:31 -0500

    Added data submission to server

--- SRv1 FrontEnd/Form.js
+++ SRv1 FrontEnd/Form.js
@@ -1,5 +1,3 @@
-const form = document.querySelector('form');
-
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -9,4 +7,32 @@ form.addEventListener('submit', (event) => {
   const message = formData.get('message');
 
   // Perform some action with the form data, such as sending an HTTP request
+});
+const form = document.querySelector('#register-form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-confirm');
+const submitBtn = document.querySelector('button[type="submit"]');
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData();
+  formData.append('username', username.value);
+  formData.append('email', email.value);
+  formData.append('password', password.value);
+  formData.append('passwordConfirm', passwordConfirm.value);
+
+  fetch('/register', {
+    method: 'POST',
+    body: formData
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
