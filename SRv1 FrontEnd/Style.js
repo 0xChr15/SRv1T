db68e62e6745620bfe0d71bf6ea9349dcf9b4690
0xChr15 <ayitzlegends34@gmail.com>
2023-02-06 01:32:56 -0500

    Styled product UI with basic js (font, head/footer

---
+++ SRv1 FrontEnd/Style.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('header');
+header.style.backgroundColor = "#333";
+header.style.color = "#fff";
+header.style.padding = "20px";
+
+const headerH1 = document.querySelector('header h1');
+headerH1.style.margin = "0";
+
+const main = document.querySelector('main');
+main.style.maxWidth = "800px";
+main.style.margin = "0 auto";
+main.style.padding = "20px";
+
+const footer = document.querySelector('footer');
+footer.style.backgroundColor = "#333";
+footer.style.color = "#fff";
+footer.style.padding = "20px";
