71d21a9da06b619d62351150b18364f683fc4849
0xChr15 <ayitzlegends34@gmail.com>
2023-02-06 03:53:57 -0500

    Added user profile display with logout button

---
+++ SRv1 FrontEnd/Profile.js
@@ -0,0 +1,4 @@
+const profileDiv = document.querySelector('#profile');
+const usernameSpan = profileDiv.querySelector('#username');
+const emailSpan = profileDiv.querySelector('#email');
+const logoutButton = profileDiv.querySelector('#logout-button');
