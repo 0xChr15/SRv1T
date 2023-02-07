8019e53b61e43e4b9bf0efe73dcd27dc9538c100
0xChr15 <ayitzlegends34@gmail.com>
2023-02-07 01:09:51 -0500

    Add Prometheus metric, alert mngmt 4 syst capacity

--- SRv1 Backend/SystemCapacityMonitor.js
+++ SRv1 Backend/SystemCapacityMonitor.js
@@ -1,6 +1,44 @@
 const Prometheus = require('prom-client');
 const capacity = new Prometheus.Gauge({
-  name: 'system_capacity',
-  help: 'System capacity in percentage',
-  labelNames: ['name']
+name: 'system_capacity',
+help: 'System capacity in percentage',
+labelNames: ['name']
+});
+
+//Set the initial capacity
+capacity.set({name: 'system'}, 50);
+
+//Update the capacity every 5 seconds
+setInterval(() => {
+capacity.set({name: 'system'}, Math.random() * 100);
+}, 5000);
+
+const alert = new Prometheus.AlertManager({
+routePrefix: '/alert',
+timeout: 10000,
+version: 0
+});
+
+alert.register({
+name: 'System capacity alert',
+type: 'threshold',
+labels: {
+severity: 'warning',
+service: 'system'
+},
+alert: 'SystemCapacity',
+expr: 'system_capacity{name="system"} > 75',
+for: '5m',
+annotations: {
+summary: 'System capacity is above 75%'
+}
+});
+
+alert.register({
+name: 'System Error alert',
+type: 'threshold',
+labels: {
+severity: 'critical',
+service: 'system'
+}
 });
