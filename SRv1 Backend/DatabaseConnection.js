4c11aa0fbe40fd0f9ac25e266bca2723a1dda031
0xChr15 <ayitzlegends34@gmail.com>
2023-02-07 00:12:39 -0500

    Added MySQL database connection, SELECT query code

---
+++ SRv1 Backend/DatabaseConnection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+//Connect to the database
+const connection = mysql.createConnection({
+    connectionLimit: 10,
+    host: "localhost:3306",
+    user: "root",
+    password: "_______",
+    database: "standaregtest1"
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+
+ //Run a SELECT query to retrieve the data
+  connection.query('SELECT name, address, dob, country_of_residence FROM <table>', (error, results) => {
+    if (error) throw error;
+
+    console.log(results);
+
+  //Disconnect from the database
+    connection.end();
+  });
+});
