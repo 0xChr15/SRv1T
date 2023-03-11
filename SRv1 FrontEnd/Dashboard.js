93bc88fbb14ad5d4434c3e273c006ee73e785a77
0xChr15 <ayitzlegends34@gmail.com>
2023-03-11 03:38:24 -0500

    New file name

--- SRv1 FrontEnd/Dashboard.js
+++ SRv1 FrontEnd/Dashboard.js
@@ -1,6 +1,14 @@
+// Check if user is logged in
+const username = localStorage.getItem("username");
+const password = localStorage.getItem("password");
+
+if (!username || !password) {
+  window.location.href = "index.html";
+}
+
+// Continue with dashboard code
 const dashboard = document.createElement("div");
 // ... code for creating the dashboard ...
-document.getElementById("dashboard").appendChild(dashboard);
 dashboard.style.backgroundColor = "#333";
 dashboard.style.color = "#fff";
 dashboard.style.width = "100%";
@@ -17,24 +25,24 @@ header.style.backgroundColor = "#444";
 dashboard.appendChild(header);
 
 const tabs = [
-{ label: "Main", link: "#main" },
-{ label: "Database(s)", link: "#databases" },
-{ label: "Performance", link: "#performance" },
-{ label: "Alerts", link: "#alerts" },
-{ label: "Calendar", link: "#calendar" },
-{ label: "Past due requirements", link: "#past-due" },
-{ label: "Scheduled Requirements", link: "#scheduled" },
-{ label: "Upcoming Requirements", link: "#upcoming" },
-{ label: "Docs", link: "#docs" }
+  { label: "Main", link: "#main" },
+  { label: "Database(s)", link: "#databases" },
+  { label: "Performance", link: "#performance" },
+  { label: "Alerts", link: "#alerts" },
+  { label: "Calendar", link: "#calendar" },
+  { label: "Past due requirements", link: "#past-due" },
+  { label: "Scheduled Requirements", link: "#scheduled" },
+  { label: "Upcoming Requirements", link: "#upcoming" },
+  { label: "Docs", link: "#docs" }
 ];
 
 tabs.forEach(tab => {
-const tabButton = document.createElement("a");
-tabButton.href = tab.link;
-tabButton.textContent = tab.label;
-tabButton.style.color = "#fff";
-tabButton.style.marginRight = "15px";
-header.appendChild(tabButton);
+  const tabButton = document.createElement("a");
+  tabButton.href = tab.link;
+  tabButton.textContent = tab.label;
+  tabButton.style.color = "#fff";
+  tabButton.style.marginRight = "15px";
+  header.appendChild(tabButton);
 });
 
 const userInfo = document.createElement("div");
@@ -45,11 +53,28 @@ userInfo.style.borderRadius = "5px";
 userInfo.style.fontFamily = "EB Garamond";
 dashboard.appendChild(userInfo);
 
-const username = "User Name";
-const userGreeting = document.createElement("h3");
-userGreeting.textContent = 'Gm, {username}';
-userGreeting.style.margin = "0";
-userInfo.appendChild(userGreeting);
+const usernameInput = document.createElement("input");
+usernameInput.setAttribute("type", "text");
+usernameInput.setAttribute("placeholder", "Please provide a 6-12 character username");
+userInfo.appendChild(usernameInput);
+
+const passwordInput = document.createElement("input");
+passwordInput.setAttribute("type", "password");
+passwordInput.setAttribute("placeholder", "Please provide a 6-12 character password");
+userInfo.appendChild(passwordInput);
+
+const submitButton = document.createElement("button");
+submitButton.textContent = "Submit";
+submitButton.addEventListener("click", () => {
+  const usernameValue = usernameInput.value;
+  const passwordValue = passwordInput.value;
+  // save the values to localStorage
+  localStorage.setItem("username", usernameValue);
+  localStorage.setItem("password", passwordValue);
+  // redirect to dashboard
+  window.location.href = "dashboard.html";
+});
+userInfo.appendChild(submitButton);
 
 const databasesSection = document.createElement("div");
 databasesSection.style.backgroundColor = "#444";
@@ -65,10 +90,6 @@ const totalDatabases = document.createElement("div");
 totalDatabases.textContent = "Total Databases: 0";
 totalDatabases.style.cursor = "pointer";
 totalDatabases.addEventListener("click", () => {
-window.location.href = "#databases";
+  window.location.href = "#databases";
 });
-databases
-
-const dashboard = document.createElement("div");
-// ... code for creating the dashboard ...
-document.getElementById("dashboard").appendChild(dashboard);
+databasesSection.appendChild(totalDatabases);
--- SRv1 FrontEnd/Login.js
+++ SRv1 FrontEnd/Login.js
@@ -15,3 +15,26 @@ const password = passwordInput.value;
 if (validUser) {
     window.location.href = "dashboard.html";
   }
+
+  const createAccountForm = document.querySelector("form");
+  createAccountForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password");
+
+    // Check if the username and password meet the requirements
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username.length >= 6 && username.length <= 12 && password.length >= 6 && password.length <= 12) {
+      // Save the credentials in localStorage
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      // Redirect to dashboard.html
+      window.location.href = "dashboard.html";
+    } else {
+      alert("Please provide a username and password that are between 6 and 12 characters.");
+    }
+  });
